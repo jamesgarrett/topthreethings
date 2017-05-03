ce550f2927ab85d3340d9b2294437d203c690348
Russ Kamalov <ruslankmlv@gmail.com>
2017-05-03 15:04:48 -0700

    Add methods to get and update task description for Day

--- TopThreeTasks/Day.swift
+++ TopThreeTasks/Day.swift
@@ -11,13 +11,40 @@ import CoreData
 
 public class Day: NSManagedObject {
 
+    enum DayTaskIndex: Int {
+        case first = 0
+        case second = 1
+        case third = 2
+    }
+
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Day> {
         return NSFetchRequest<Day>(entityName: "Day")
     }
 
     @NSManaged public var date: NSDate
-    @NSManaged public var tasks: Set<Task>
-    
+    @NSManaged private var tasks: Set<Task>
+
+    private func task(with index: DayTaskIndex) -> Task {
+        guard let existingTask = tasks.first(where: { $0.id == Int16(index.rawValue) }) else {
+            let newTask = Task(context: managedObjectContext!)
+            newTask.id = Int16(index.rawValue)
+            newTask.day = self
+            return newTask
+        }
+        return existingTask
+    }
+
+    func descriptionOfTask(at index: DayTaskIndex) -> String? {
+        return task(with: index).taskDescription
+    }
+
+    func updateTask(at index: DayTaskIndex, with text: String) {
+        let taskToUpdate = task(with: index)
+        taskToUpdate.taskDescription = text
+    }
+
+}
+
 extension Day: Managed {
     static var defaultSortDescriptors: [NSSortDescriptor] {
         return [NSSortDescriptor(key: "date", ascending: false)]
