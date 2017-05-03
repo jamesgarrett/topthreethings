0dd9c8a45b6f02dd9feba9bf4935a54938ef1b88
Russ Kamalov <ruslankmlv@gmail.com>
2017-05-03 12:56:38 -0700

    Add ColorPalette

---
+++ TopThreeTasks/ColorPalette.swift
@@ -0,0 +1,14 @@
+//
+//  ColorPalette.swift
+//  TopThreeTasks
+//
+//  Created by Ruslan Kamalov on 5/3/17.
+//  Copyright © 2017 Ruslan Kamalov. All rights reserved.
+//
+
+import UIKit
+
+enum ColorPalette {
+    static let inactiveGrayColor = UIColor(white: 88.0/255.0, alpha: 1.0)
+    static let activeGrayColor = UIColor(white: 55.0/255.0, alpha: 1.0)
+}
